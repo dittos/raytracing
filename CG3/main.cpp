7d2bd12d0b804d896b9963f60432f9993631edef
Taeho Kim <dittos@gmail.com>
2015-04-29 12:59:56 +0900

    Initial Commit

---
+++ CG3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CG3
+//
+//  Created by 김태호 on 2015. 4. 29..
+//  Copyright (c) 2015년 김태호. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
